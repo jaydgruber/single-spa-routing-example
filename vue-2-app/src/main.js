53d701c38712bc0f6db850371b944fd0a0d316a7
jay.gruber <jay.gruber@avalara.com>
2021-09-23 14:55:57 -0700

    working example

--- vue-2-app/src/main.js
+++ vue-2-app/src/main.js
@@ -1,14 +1,27 @@
-import './set-public-path';
 import Vue from 'vue';
+import singleSpaVue from 'single-spa-vue';
+
 import App from './App.vue';
 import router from './router';
-import singleSpaVue from 'single-spa-vue';
+
+Vue.config.productionTip = false;
 
 const vueLifecycles = singleSpaVue({
   Vue,
   appOptions: {
     render(h) {
-      return h(App);
+      return h(App, {
+        props: {
+          // single-spa props are available on the "this" object. Forward them to your component as needed.
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
+          /*
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
+          */
+        },
+      });
     },
     router,
   },
