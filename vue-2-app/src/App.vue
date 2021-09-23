d2964b3e5828ec4c97cf3a190910cc00375b595b
jay.gruber <jay.gruber@avalara.com>
2021-09-23 14:51:56 -0700

    adding stuff

--- vue-2-app/src/App.vue
+++ vue-2-app/src/App.vue
@@ -1,28 +1,18 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Nav from '@/components/Nav.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: { Nav },
+    setup() {
+    },
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+    <div>
+      <h2>vue-2-app</h2>
+      <Nav />
+
+      <router-view/>
+    </div>
+</template>
--- vue-2-app/src/main.js
+++ vue-2-app/src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import './set-public-path';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import singleSpaVue from 'single-spa-vue';
 
-Vue.config.productionTip = false
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h) {
+      return h(App);
+    },
+    router,
+  },
+});
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
